26200cdd0f2d68c6ba74cb4e81be682e45902da6
christophe <christophe@bertrand-feilen.ch>
2017-05-09 11:28:28 +0200

    timed kv store response

--- automatedClients/automated.go
+++ automatedClients/automated.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -14,99 +12,119 @@ import (
 	"time"
 )
 
-var in *bufio.Reader
-var out io.Writer
 var put = "PUT"
 var get = "GET"
 
 type httphandler struct {
-	ok chan<- bool
+	ok chan<- time.Time
 	id int
 }
 
+//Listens to the responses of the kv store
 func (h *httphandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	id := r.RequestURI
 
 	id = strings.TrimPrefix(id, "/")
-	fmt.Println(id)
 	// fmt.Println("recieved ok at " + strconv.Itoa(h.id))
 	switch {
-	case r.Method == "PUT":
+	case r.Method == put:
 		_, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Printf("Failed to read on PUT (%v)\n", err)
 			http.Error(w, "Failed on PUT", http.StatusBadRequest)
 			return
 		}
-		h.ok <- true
+		h.ok <- time.Now()
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
 
 	}
 }
-func newClient(destPort, retPort int, end chan<- bool) {
-	ok := make(chan bool)
+
+func newClient(destPort, retPort int, t time.Duration, end chan<- bool) {
+	ok := make(chan time.Time)
+	defer close(ok)
 	returnAddr := "http://127.0.0.1:" + strconv.Itoa(retPort)
 	createListener(retPort, ok)
 	timeout := make(chan bool, 1)
-	go func() {
-		time.Sleep(30 * time.Second)
+	go func(t time.Duration) { //terminate client after t second
+		time.Sleep(t * time.Second)
 		timeout <- true
-	}()
+	}(t)
+	var sendTime time.Time
+	totalDuration := time.Duration(0)
+	var numReq int
 	for {
 		select {
 		case <-timeout:
-			fmt.Println("finished execution")
+			fmt.Printf("average time to finish the execution is %v\n", totalDuration / time.Duration(numReq))
 			end <- true
-
-		case <-ok:
-			time.Sleep(500 * time.Millisecond)
-			destAddr := "http://127.0.0.1:" + strconv.Itoa(destPort)
-			key := strconv.Itoa(rand.Int())
-			value := key
-			fmt.Println("send")
-			sendRequest(put, key, value, destAddr, returnAddr)
+			break
+		case receiveTime, open := <-ok:
+			if open {
+				if !receiveTime.Equal(time.Unix(0, 0)) {
+					reqTime := receiveTime.Sub(sendTime)
+					totalDuration = reqTime + totalDuration
+					numReq++
+					fmt.Printf("time to respond %v\n ", reqTime)
+				}
+				//time.Sleep(500 * time.Millisecond)
+				destAddr := "http://127.0.0.1:" + strconv.Itoa(destPort)
+				key := strconv.Itoa(rand.Int())
+				value := key
+				sendTime = time.Now()
+				sendRequest(put, key, value, destAddr, returnAddr)
+			} else {
+				fmt.Println("terminiating client " + strconv.Itoa(retPort))
+				end <- true
+				return
+			}
 		}
 	}
 }
 
-func createListener(port int, ok chan<- bool) {
+func createListener(port int, ok chan<- time.Time) {
 	srv := http.Server{
 		Addr:    ":" + strconv.Itoa(port),
 		Handler: &httphandler{ok, port},
 	}
 	go func() {
-		ok <- true
+		ok <- time.Unix(0, 0) //for startup
 		if err := srv.ListenAndServe(); err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
+			close(ok)
 		}
 	}()
 }
 
 func main() {
-	numberClients := 5
+	numberClients := 50
 	destPrefix := 11380
+	t := time.Duration(10)
 	wait := make(chan bool)
+	defer close(wait)
 	for client := 1; client <= numberClients; client++ {
-		fmt.Println("new client")
+		fmt.Printf("new client %v \n", client)
 		retPort := client + 40005
 		destPort := destPrefix + (client%3)*1000
-		go newClient(destPort, retPort, wait)
+		go newClient(destPort, retPort, t, wait)
 	}
-	<-wait
+	for i := 1; i <= numberClients; i++ {
+		<-wait
+	}
 }
 
 func sendRequest(method, key, value, destAddr, returnAddr string) {
 	if method == put {
 		req, _ := http.NewRequest("PUT", destAddr+"/"+key+"/"+value, bytes.NewBufferString(returnAddr))
 		client := &http.Client{}
-		r, err := client.Do(req)
+		_, err := client.Do(req)
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println(r.Body)
+			// fmt.Println(r.Body)
 		}
-		fmt.Println("sending to " + destAddr)
+		// fmt.Println("sending to " + destAddr)
 	} else {
 		resp, err := http.Get(destAddr + "/" + key)
 		if err != nil {
